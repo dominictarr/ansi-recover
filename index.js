6243f8b3bf9d452e55866fa966a67c4b3afc2545
Dominic Tarr <dominic.tarr@gmail.com>
2013-12-11 00:46:00 +0800

    support mouse

--- index.js
+++ index.js
@@ -6,26 +6,41 @@
 module.exports = function (opts) {
   opts = opts || {}
   var start = [
-      '\u001b[?1049h', //clear the screen from previous usage
-      '\u001b[?12l',   //this is needed to clear the terminal
-      '\u001b[H',      //move cursor to the top left
+      '\x1b[?1049h', //clear the screen from previous usage
+      '\x1b[?12l',   //this is needed to clear the terminal
+      '\x1b[H',      //move cursor to the top left
       //hide cursor, but only if we have a TTY,
       //because otherwise it doesn't seem to come back
-      process.stdout.isTTY && opts.cursor === false ? '\u001b[?25l' : '',
+      process.stdout.isTTY && opts.cursor === false ? '\x1b[?25l' : '',
+      opts.mouse ? '\x1b[?1000h' : ''
   ].join('')
 
   var exit = '\u001b[?1049l' //recover the initial screen
 
   process.stdout.write(start)
 
+  var processExit = process.exit
+
+  var exited = false
   function show () {
-    process.stdout.write('\u001b[0m')   //plain, non coloured terminal.
-    //very strange. this only works if it's in a separate write call to the final code
-    if(opts.cursor === false)
-      process.stdout.write('\u001b[?25h') //show cursor
-    process.stdout.write(exit)
-    process.exit()
+    if(!exited) {
+      exited = true
+      process.stdout.write('\u001b[0m')   //plain, non coloured terminal.
+      //very strange. this only works if it's in a separate write call to the final code
+      if(opts.cursor === false)
+        process.stdout.write('\u001b[?25h') //show cursor
+
+      if(opts.mouse)
+        process.stdout.write('\x1b[?1000l')
+
+      process.stdout.write(exit)
+    }
+    setTimeout(function () {
+      processExit.call(process)
+    }, 50)
   }
+
+  process.exit = show
 
   process
     .on('exit', show)
